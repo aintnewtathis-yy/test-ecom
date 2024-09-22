c42507f3ff9697283c4e48e253050cd8437890b1
aintnewtathis-yy <aintnewtathis@mail.ru>
2024-09-22 11:36:17 +0300

    asd

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -1,7 +1,8 @@
-/** @type {import('./$types').PageServerLoad} */
+import { CMS_URL } from '$env/static/private';
+
 export async function load({ fetch }) {
 	try {
-		const products = await fetch('http://localhost:1337/api/products?populate=*');
+		const products = await fetch(`${CMS_URL}/api/products?populate=*`);
 		const productsData = await products.json();
 
 		return {
