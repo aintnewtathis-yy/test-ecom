eb08eda0542b9d17667c96ee46ede4a4549f5e10
aintnewtathis-yy <aintnewtathis@mail.ru>
2024-09-27 23:15:15 +0300

    123

--- src/lib/yookassa.js
+++ src/lib/yookassa.js
@@ -35,22 +35,18 @@ export const initialPayment = async (productsData) => {
 			save_payment_method: 'false'
 		};
 
-		// Await the response from the fetch call
 		const response = await fetch(url, {
 			method: 'POST',
 			headers: headers,
 			body: JSON.stringify(params)
 		});
 
-		// Handle HTTP error response
 		if (!response.ok) {
 			throw new Error(`HTTP error: ${response.status}`);
 		}
 
-		// Parse the response JSON
 		const res = await response.json();
 
-		// Return the URL and order ID as an object
 		return {
 			url: res.confirmation.confirmation_url,
 			id: res.id
@@ -58,15 +54,16 @@ export const initialPayment = async (productsData) => {
 	} catch (err) {
 		console.warn('ERROR', err);
 
-		// Throw a custom error
-		throw new Error(400, 'error');
+		throw error(400, {
+			message: 'Error in making order',
+			details: err.message
+		});
 	}
 };
 
 export const confirmPayment = async (order_id) => {
 	console.log(order_id);
 	try {
-		// Step 1: Update order status in the CMS to "paid"
 		const responseOrder = await fetch(`${CMS_URL}/api/orders/updateOrderStatus`, {
 			method: 'POST',
 			headers: { 'Content-Type': 'application/json' },
@@ -85,7 +82,6 @@ export const confirmPayment = async (order_id) => {
 		const responseOrderData = await responseOrder.json();
 		console.log('Order status updated:', responseOrderData);
 
-		// Step 2: Confirm the payment in YooKassa (Capture the payment)
 		const responsePayment = await fetch(`https://api.yookassa.ru/v3/payments/${order_id}/capture`, {
 			method: 'POST',
 			headers: {
@@ -120,7 +116,6 @@ export const confirmPayment = async (order_id) => {
 export const cancelPayment = async (order_id) => {
 	console.log(order_id);
 	try {
-		// Step 1: Update order status in the CMS to "paid"
 		const responseOrder = await fetch(`${CMS_URL}/api/orders/updateOrderStatus`, {
 			method: 'POST',
 			headers: { 'Content-Type': 'application/json' },
@@ -137,7 +132,6 @@ export const cancelPayment = async (order_id) => {
 		const responseOrderData = await responseOrder.json();
 		console.log('Order status updated:', responseOrderData);
 
-		// Step 2: Confirm the payment in YooKassa (Capture the payment)
 		const responsePayment = await fetch(`https://api.yookassa.ru/v3/payments/${order_id}/cancel`, {
 			method: 'POST',
 			headers: {
@@ -168,114 +162,3 @@ export const cancelPayment = async (order_id) => {
 		});
 	}
 };
-
-/* 
-exports.UkassaWebHook = functions.https.onRequest(async (request, response) => {
-	if (request.body.event == 'payment.waiting_for_capture') {
-		let payment_id = request.body.object.id;
-		let status = request.body.object.status;
-		if (status == 'waiting_for_capture') {
-			// сюда попадаем, если клиент оплатил
-			await confirmPayment(payment_id);
-			await getPayment(payment_id);
-		}
-	}
-	response.send('OK');
-});
-
-const confirmPayment = async (payment_id) => {
-	await admin
-		.firestore()
-		.collection('orders')
-		.where('payment_id', '==', payment_id)
-		.limit(1)
-		.get()
-		.then((snapshot) => {
-			if (snapshot.size > 0) {
-				const firstDoc = snapshot.docs[0].ref;
-				firstDoc
-					.update({ paid: true })
-					.then(() => {
-						console.log('Документ успешно обновлен');
-					})
-					.catch((err) => {
-						console.log('Ошибка обновления документа', err);
-					});
-			} else {
-				console.log('документы не найдены');
-			}
-		})
-		.catch((err) => {
-			console.log('Ошибка получения документа', err);
-			return null;
-		});
-};
-
-const getPayment = async (payment_id) => {
-	const url = `https://api.yookassa.ru/v3/payments/${payment_id}/capture`;
-
-	var headers = {
-		Authorization: authorization,
-		'Idempotence-Key': uuidv4().toString(),
-		'Content-Type': 'application/json'
-	};
-
-	return await axios
-		.post(
-			url,
-			{},
-			{
-				headers: headers
-			}
-		)
-		.then((res) => res.data)
-		.then(async (res) => {
-			functions.logger.log('Платеж успешно подтвержден', res);
-			return true;
-		})
-		.catch((err) => {
-			functions.logger.log('Ошибка при подтверждении платежа', err);
-			return false;
-		});
-};
-
-const cancelPayemnt = async (payment_id) => {
-	const url = `https://api.yookassa.ru/v3/payments/${payment_id}/cancel`;
-
-	var headers = {
-		Authorization: authorization,
-		'Idempotence-Key': uuidv4().toString(),
-		'Content-Type': 'application/json'
-	};
-
-	return await axios
-		.post(
-			url,
-			{},
-			{
-				headers: headers
-			}
-		)
-		.then((res) => res.data)
-		.then(async (res) => {
-			functions.logger.log('Платеж успешно отменен', res);
-			return true;
-		})
-		.catch((err) => {
-			functions.logger.log('Ошибка при отмене платежа', err);
-			return false;
-		});
-};
-
-exports.getPaymentApi = functions.https.onRequest(async (request, response) => {
-	var payment_id = request.body.payment_id;
-	await getPayment(payment_id);
-	response.status(200);
-});
-
-exports.cancelPaymentApi = functions.https.onRequest(async (request, response) => {
-	var payment_id = request.body.payment_id;
-	await cancelPayemnt(payment_id);
-	response.status(200);
-});
- */
